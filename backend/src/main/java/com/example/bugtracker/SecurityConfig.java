c9e07f16abb54fe3f2e4e3306d2fcd3d9334c5a4
Martin Dúbravka <matodoubravka@gmail.com>
2024-10-31 12:46:41 +0100

    commit before index api

--- backend/src/main/java/com/example/bugtracker/SecurityConfig.java
+++ backend/src/main/java/com/example/bugtracker/SecurityConfig.java
@@ -2,12 +2,13 @@ package com.example.bugtracker;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import com.example.bugtracker.security.JwtAuthenticationFilter;
 
 
@@ -28,7 +29,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable()) 
             .authorizeHttpRequests(authz -> authz
-                .requestMatchers("/auth/sign-up","/auth/sign-in", "/auth/refresh").permitAll() 
+                .requestMatchers("/index.html", "/static/**","/auth/sign-up","/auth/sign-in", "/auth/refresh").permitAll() 
                 .anyRequest().authenticated() 
             )
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); 
--- backend/src/main/java/com/example/bugtracker/security/JwtAuthenticationFilter.java
+++ backend/src/main/java/com/example/bugtracker/security/JwtAuthenticationFilter.java
@@ -1,25 +1,24 @@
 package com.example.bugtracker.security;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.GrantedAuthority;
 
-
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.FilterChain;
-import java.util.List;
-import java.util.Map;
-
-import java.util.Collections;
 import com.example.bugtracker.service.RedisService;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
 @Component
@@ -35,7 +34,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         throws ServletException, IOException {
 
             String path = request.getRequestURI();
-            if(path.contains("/auth/sign-in") || path.contains("/auth/sign-up") || path.contains("/auth/refresh")) {
+            if(path.contains("/auth/sign-in") || path.contains("/auth/sign-up") || path.contains("/auth/refresh") || path.contains("/index.html")) {
                 filterChain.doFilter(request, response);
                 return;
             }
